e8fb8071e7704ed064531ef8fd7fd46082c953e4
Alex González <alejandrogsa@gmail.com>
2024-03-03 15:00:45 -0300

    add change to test ci cache

--- front-end-app/src/App.tsx
+++ front-end-app/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Change to cache</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
